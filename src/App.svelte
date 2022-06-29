20a835842fd631014182f4d965c58b6d4471174c
m r <trash89@laposte.net>
2022-06-29 13:02:17 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Navbar from "./Navbar.svelte";
+  let fruits = ["apple", "orange", "lemon"];
+</script>
+
+<!-- <style></style> -->
+<!-- <CSS/STYLING -->
+
+<Navbar />
+
+{#each fruits as item}
+  <h1>fruit : {item}</h1>
+{/each}
